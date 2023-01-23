273b408f1dbe0a1898bfd1eef859d76ee69a53f2
Jackson Weibel <weibel.jackson@gmail.com>
2023-01-22 20:09:18 -0600

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CompleteControl.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="CompleteControl")
+
+public class CompleteControl extends LinearOpMode{
+    private DcMotor leftFront;
+    private DcMotor leftRear;
+    private DcMotor rightFront;
+    private DcMotor rightRear;
+
+    private DcMotor leftSlide;
+    private CRServo claw;
+
+    @Override
+    public void runOpMode() {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftRear = hardwareMap.get(DcMotor.class, "leftRear");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+
+        //leftSlide = hardwareMap.get(DcMotor.class, "leftSlide");
+        //claw = hardwareMap.get(CRServo.class, "claw");
+        // Put initialization blocks here.
+        //motor.setPower(0);
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                // Robot Movement Code
+                double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+                double robotAngle = Math.atan2(gamepad1.left_stick_y, -1*gamepad1.left_stick_x) - Math.PI/4;
+                double rightX = -1*gamepad1.right_stick_x;
+                final double v1 = r*Math.cos(robotAngle) + rightX;
+                final double v2 = r*Math.sin(robotAngle) - rightX;
+                final double v3 = r*Math.sin(robotAngle) + rightX;
+                final double v4 = r*Math.cos(robotAngle) - rightX;
+                leftFront.setPower(v1);
+                rightFront.setPower(v2);
+                leftRear.setPower(v3);
+                rightRear.setPower(-v4);
+
+                // Robot Arm Code
+//                if (gamepad2.left_trigger>0) {
+//                    leftSlide.setPower(gamepad2.left_trigger);
+//                }
+//                else if (gamepad2.right_trigger>0) {
+//                    leftSlide.setPower(-gamepad2.right_trigger);
+//                }
+//                else {
+//                    leftSlide.setPower(0);
+//                }
+//                if (gamepad2.x){
+//                    claw.setPower(0.05f);
+//                }
+//                else if (gamepad2.b){
+//                    claw.setPower(-0.05f);
+//                }
+//                else {
+//                    claw.setPower(0f);
+//                }
+                telemetry.update();
+            }
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/testing/ParkingTest.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.testing;
+
+import android.widget.Switch;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.opmode.vision.SleeveDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous(group="testing")
+public class ParkingTest extends LinearOpMode {
+
+    private SleeveDetection sleeveDetection;
+    private OpenCvCamera camera;
+
+    // Name of the Webcam to be set in the config
+    private String webcamName = "Webcam 1";
+
+    private Vector2d leftParking = new Vector2d();
+    private Vector2d centerParking = new Vector2d();
+    private Vector2d rightParking = new Vector2d();
+    private Vector2d targetParking;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        //vision init
+        {
+            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+            camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+            sleeveDetection = new SleeveDetection();
+            camera.setPipeline(sleeveDetection);
+
+            camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+                @Override
+                public void onOpened() {
+                    camera.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+                }
+
+                @Override
+                public void onError(int errorCode) {
+                }
+            });
+            SleeveDetection.ParkingPosition parkingPosition = sleeveDetection.getPosition();
+            switch(parkingPosition)
+            {
+                case CENTER:
+                    targetParking = centerParking;
+                    break;
+                case RIGHT:
+                    targetParking = rightParking;
+                    break;
+                default:
+                    targetParking = leftParking;
+            }
+
+            while (!isStarted()) {
+                telemetry.addData("ROTATION: ", sleeveDetection.getPosition());
+                telemetry.update();
+            }
+        }
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Pose2d startPose = new Pose2d(-65, 32, 0);
+
+        drive.setPoseEstimate(startPose);
+
+        Trajectory traj = drive.trajectoryBuilder(startPose)
+                .strafeLeft(28)
+                .strafeRight(28)
+                .splineTo(targetParking, Math.toRadians(90))
+                .build();
+
+        drive.followTrajectory(traj);
+    }
+}
